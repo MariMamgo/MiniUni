51e2bad84dc1c32e573f0f89bc8775d77fb1e6f0
Annablaa <okropiridze.ani@kiu.edu.ge>
2026-02-13 19:06:29 +0400

    Add project files

---
+++ rewrite-author.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export GIT_AUTHOR_NAME='Annablaa'
+export GIT_AUTHOR_EMAIL='okropiridze.ani@kiu.edu.ge'
+export GIT_COMMITTER_NAME='Annablaa'
+export GIT_COMMITTER_EMAIL='okropiridze.ani@kiu.edu.ge'
